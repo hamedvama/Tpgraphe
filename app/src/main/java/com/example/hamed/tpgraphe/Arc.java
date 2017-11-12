c81ce7c3733d070cf8b83687023dfc15a83dc025
hamedvama <32411898+hamedvama@users.noreply.github.com>
2017-11-12 13:53:37 +0100

    version 1.2 12/11/2017

--- app/src/main/java/com/example/hamed/tpgraphe/Arc.java
+++ app/src/main/java/com/example/hamed/tpgraphe/Arc.java
@@ -1,5 +1,10 @@
 package com.example.hamed.tpgraphe;
 
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Path;
+
 /**
  * Created by hamed on 11/10/2017.
  */
@@ -7,13 +12,106 @@ package com.example.hamed.tpgraphe;
 public class Arc {
 
     private Node noeudorigin;
-    private Node noeudextremit;
+    private Node noeudefin;
+    private int Xorig, Yorig, Xfin, Yfin;
+    private int midX, midY;
+    private Paint paint;
+    private String color = "RED";
+
+    public Arc(Node nodorig, Node nodext) {
+
+        this.noeudorigin = nodorig;
+        this.noeudefin = nodext;
+
+        this.Xorig = nodorig.getX();
+        this.Xfin = nodext.getX();
+        this.Yorig = nodorig.getY();
+        this.Yfin = nodext.getY();
+
+        midX = (nodorig.getX()+nodext.getX())/2;
+        midY = (nodorig.getY()+nodext.getY())/2;
+        paint = new Paint();
+        this.paint.setColor(Color.parseColor(color));
+
+    }
+
+    public int getXorig() {
+        return Xorig;
+    }
+
+    public void setXorig(int xorig) {
+        Xorig = xorig;
+    }
+
+    public int getYorig() {
+        return Yorig;
+    }
+
+    public void setYorig(int yorig) {
+        Yorig = yorig;
+    }
+
+    public int getXfin() {
+        return Xfin;
+    }
+
+    public void setXfin(int xfin) {
+        Xfin = xfin;
+    }
+
+    public int getYfin() {
+        return Yfin;
+    }
+
+    public void setYfin(int yfin) {
+        Yfin = yfin;
+    }
+
+    public int getMidX() {
+        return midX;
+    }
+
+    public void setMidX(int midX) {
+        this.midX = midX;
+    }
+
+    public int getMidY() {
+        return midY;
+    }
+
+    public void setMidY(int midY) {
+        this.midY = midY;
+    }
 
     public Node getNoeudorigin() {
         return noeudorigin;
     }
 
-    public Node getNoeudextremit() {
-        return noeudextremit;
+    public Node getNoeudefin() {
+        return noeudefin;
+    }
+
+    public Paint getPaint() {
+        return paint;
+    }
+
+    public void setPaint(Paint paint) {
+        this.paint = paint;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public void setNoeudorigin(Node noeudorigin) {
+        this.noeudorigin = noeudorigin;
+    }
+
+    public void setNoeudefin(Node noeudefin) {
+        this.noeudefin = noeudefin;
     }
 }
